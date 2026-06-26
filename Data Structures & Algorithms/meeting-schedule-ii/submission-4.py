1ff977c5cf98850cde149c21e24d2cf3adf3532c
Javlonbek Kosimov <68327261+javydevx@users.noreply.github.com>
2026-06-26 21:26:24 +0200

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,29 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        
+        n = len(intervals)
+        max_rooms = 0
+        
+        for i in range(n):
+            start_time = intervals[i].start
+            rooms_needed = 0
+
+            for j in range(n):
+                # Check if meeting j is happening when meeting i starts
+                if intervals[j].start <= start_time < intervals[j].end:
+                    rooms_needed += 1
+            
+            max_rooms = max(max_rooms, rooms_needed)
+        
+        return max_rooms
+
+
